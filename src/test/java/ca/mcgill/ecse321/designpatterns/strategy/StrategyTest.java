ac0fd6c2213be088266572be40ba4dd1ec7046e0
Chris Li <chris1e686@gmail.com>
2018-10-24 16:31:44 -0400

    Add a RandomDiscount to StrategyTest.java

--- src/test/java/ca/mcgill/ecse321/designpatterns/strategy/StrategyTest.java
+++ src/test/java/ca/mcgill/ecse321/designpatterns/strategy/StrategyTest.java
@@ -1,4 +1,4 @@
-package org.mcgill.ecse321.designpatterns.strategy;
+package ca.mcgill.ecse321.designpatterns.strategy;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +13,9 @@ public class StrategyTest {
 		
 		discount = new PercentDiscount(25);
 		assertEquals(discount.compute(100), 75, 0.001);
+		
+		discount = new RandomDiscount();
+		assertTrue(discount.compute(100)>=50);
+		assertTrue(discount.compute(100)<=99);
 	}
 }
